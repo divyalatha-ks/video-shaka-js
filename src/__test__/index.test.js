66670523d1f342231998374f1aed56e80fd1d467
Divya Latha <dsreenivasulu@newrelic.com>
2026-01-22 20:05:16 +0530

    feat: Implement shared mock infrastructure from video-core - Update dependency to published npm version 4.1.5-beta - Configure jest to use shared mock from video-core package - Remove manual mock files (now using centralized mock) - Achieve 100% test coverage with shared infrastructure

--- src/__test__/index.test.js
+++ src/__test__/index.test.js
@@ -1,15 +1,14 @@
 import nrvideo from '@newrelic/video-core'
 import Tracker from "../tracker";
-import ShakaTracker from "../index";
+
+const exportedModule = require("../index");
 
 describe("nrvideo ShakaTracker Assignment", () => {
   it("should assign Tracker to nrvideo.ShakaTracker", () => {
-    // The ShakaTracker should be the same as our Tracker class
-    expect(ShakaTracker).toBe(Tracker);
+    expect(nrvideo.ShakaTracker).toBe(Tracker);
   });
 
-  it("should export ShakaTracker as default export", () => {
-    expect(ShakaTracker).toBe(Tracker);
-    expect(typeof ShakaTracker).toBe('function');
+  it("should export the modified nrvideo object", () => {
+    expect(exportedModule).toBe(nrvideo);
   });
 });
